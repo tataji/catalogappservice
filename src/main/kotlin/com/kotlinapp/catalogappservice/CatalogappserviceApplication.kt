259ec7f58e4a6180bb0edd4435b8e988770bc2f5
Tataji Kondepudi <kondepudi.tataji@gmail.com>
2024-06-02 11:11:30 +0530

    Initial commit

---
+++ src/main/kotlin/com/kotlinapp/catalogappservice/CatalogappserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlinapp.catalogappservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CatalogappserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CatalogappserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/kotlinapp/catalogappservice/CatalogappserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinapp.catalogappservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CatalogappserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
